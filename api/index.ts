fa7444f93ff6a6e69d670f13fad279226ba46eee
TalesEdu323 <taleseduardo45@gmail.com>
2026-07-13 13:53:07 -0300

    route api requests to serverless handler

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,9 @@
 import { createApp } from "../server/_core/index";
 
+export const config = {
+  runtime: "nodejs",
+};
+
 export default async function handler(req: any, res: any) {
   const { app } = await createApp({ serveFrontend: false });
   return app(req, res);
